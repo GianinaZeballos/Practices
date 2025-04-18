c3b7a754f1baa01372d9711b9b8b9d230a68bf8e
giani <gianina.ze.ro@gmail.com>
2025-04-17 21:53:46 -0300

    otros ejercicios mas

---
+++ src/Modulo1/InvertirNumero010.java
@@ -0,0 +1,13 @@
+package Modulo1;
+/*Escribe una función en Java llamada invertirNumero que tome un número entero como parámetro y
+devuelva el número invertido. Por ejemplo, si la entrada es 12345, la función debería devolver 54321.*/
+
+import static utils.Escaner.entrada;
+
+public class InvertirNumero010 {
+    public static void invertirNum() {
+        System.out.println("Ingrese un numero: ");
+        int numero = entrada.nextInt();
+        for (int i=
+    }
+}
